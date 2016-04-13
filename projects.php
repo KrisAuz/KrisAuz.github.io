31dbdecf30d174727094a64163a01e0a9fef85e4
KrisAuz <kristapsaaa@gmail.com>
2016-04-13 10:36:07 +0100

    Edited redirect

--- projects.php
+++ projects.php
@@ -1,7 +1,33 @@
 <!doctype html>
-<html>
-<head><head>
-<body>
-<a href="https://www.dropbox.com/sh/vdo8ez1thr76ts8/AACHzYj54l25j0KO03PVC76oa?dl=0" class="large button">GO</a>
-<body>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Kristaps Auzins - web developer, technology enthusiast. This is my about/CV page. Blog/portfolio coming in the furure.">
+    <meta name="keywords" content="Kristaps Auzins, web development, technology, HTML, CSS, JavaScript">
+    <meta name="author" content="Kristaps Auzins">
+    <title> Kristaps Auzins | Welcome</title>
+    <link rel="stylesheet" href="css/foundation.min.css" />
+    <script src="js/vendor/modernizr.js"></script>
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/foundation-icons/foundation-icons.css" />
+    <link rel="shortcut icon" href="/img/KA.ico">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Righteous&amp;.subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <body>
+    <a href="https://www.dropbox.com/sh/vdo8ez1thr76ts8/AACHzYj54l25j0KO03PVC76oa?dl=0" class="large button">GO</a>
+     <script src="js/foundation.min.js"></script>
+    
+    <script>
+        $(document).foundation();
+    </script>
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-68263195-1', 'auto');
+    ga('send', 'pageview');
+    </script>
+  </body>
 </html>
