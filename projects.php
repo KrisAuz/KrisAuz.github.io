352809e88b5b4d9adde76c55c0ff19121096e748
KrisAuz <kristapsaaa@gmail.com>
2016-04-13 10:28:31 +0100

    Redirect

--- projects.php
+++ projects.php
@@ -1,5 +1,6 @@
 <!doctype html>
+<html>
 <head>
-<meta http-equiv="refresh" content="0; URL='https://www.dropbox.com/sh/vdo8ez1thr76ts8/AACHzYj54l25j0KO03PVC76oa?dl=0'" />
-</head>
+<meta http-equiv="refresh" content="0; url=https://www.dropbox.com/sh/vdo8ez1thr76ts8/AACHzYj54l25j0KO03PVC76oa?dl=0">
+<head>
 </html>
