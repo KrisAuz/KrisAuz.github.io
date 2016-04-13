4bc19e3bb5612abb6f9f4eb99259eb44d7fdac58
KrisAuz <kristapsaaa@gmail.com>
2016-04-13 10:21:00 +0100

    Missed a "

--- projects.php
+++ projects.php
@@ -1,5 +1,5 @@
 <!doctype html>
 <head>
-<script>window.location = "https://www.dropbox.com/sh/vdo8ez1thr76ts8/AACHzYj54l25j0KO03PVC76oa?dl=0</script>
+<script>window.location = "https://www.dropbox.com/sh/vdo8ez1thr76ts8/AACHzYj54l25j0KO03PVC76oa?dl=0"</script>
 </head>
 </html>
